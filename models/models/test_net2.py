8c1b5d01f388df001b8c6ce9aa8c28f4b2976a92
weijie chen <374780732weijie@gmail.com>
2022-07-09 15:09:42 -0400

    more model

--- models/models/test_net2.py
+++ models/models/test_net2.py
@@ -4,11 +4,13 @@ import torch.nn.functional as F
 
 class TestNet2(nn.Module):
     def __init__(self):
-        super.__init__()
-        self.conv1 = nn.Conv2d(3,32,4)
-        self.conv2 = nn.Conv2d(32,64,4)
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,32,3)
+        self.conv2 = nn.Conv2d(32,64,3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
         
-        num_out_nur = 32
+        num_out_nur = 256
         self.fc1 = nn.Linear(num_out_nur, num_out_nur//2)
         self.fc2 = nn.Linear(num_out_nur//2, 1)
         
@@ -21,6 +23,16 @@ class TestNet2(nn.Module):
         x = F.max_pool2d(
             F.relu(self.conv2(x)),
             (3,3)
+        )
+
+        x = F.avg_pool2d(
+            F.relu(self.conv3(x)),
+            (3,3)
+        )
+
+        x = F.avg_pool2d(
+            F.tanh(self.conv4(x)),
+            (2,2)
         )
         return x
         
--- testing_model2.py
+++ testing_model2.py
@@ -1,4 +1,5 @@
 from cgi import test
+from numpy import dtype
 from pkg_resources import working_set
 import torch
 import torch.nn as nn
@@ -30,15 +31,14 @@ mean = torch.tensor(cat_dog_mean).to(device).view(3,1,1)
 std  = torch.tensor(cat_dog_std).to(device).view(3,1,1)
 
 transform = transforms.Compose([
-    transforms.Resize((350,350)),
+    transforms.Resize((150,150)),
     transforms.ToTensor(),
-    #transforms.GaussianBlur(kernel_size=(15,15), sigma=(0.1, 0.2)),
     transforms.Normalize(cat_dog_mean, cat_dog_std),
 ])
 
 data_dir = os.path.join(".","data")
-batch_size = 64
-epochs = 15
+batch_size = 2
+epochs = 20000
 
 test1_dataset = TestingModel1DataSet(data_dir=data_dir, transform=transform)
 train_size = int(len(test1_dataset) * 0.80)
@@ -49,11 +49,11 @@ train_dataset, test_dataset = random_split(test1_dataset, [train_size, test_size
 dataloader_train = DataLoader(train_dataset, batch_size=batch_size,shuffle=True)
 dataloader_test = DataLoader(test_dataset, batch_size=batch_size,shuffle=False)
 
-net = TestNet1().to(device)
+net = TestNet2().to(device)
 loss_fn = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr = 0.002)
+optimizer = optim.Adam(net.parameters(), lr = 0.2)
 #lambda_fun = lambda epoch : 1/( 10 ** epoch)
-lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=1/math.e, patience=3, verbose=True)
+lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer,gamma=0.9)
 
 model_storage_dir = os.path.join(".","models","checkpoints",str(datetime.now().strftime("%m-%d-%Y-%H-%M-%S")))
 os.mkdir(model_storage_dir)
@@ -61,50 +61,56 @@ shutil.copy2(os.path.join(".","models","models","test_net2.py"), os.path.join(mo
 
 
 if __name__ == "__main__":
+    for sample in dataloader_train:
+        X,y = sample
+        break
+    #X, y = next(iter(dataloader_train))
+    X, y = X.to(device), y.to(device).float()
     start = time()
     print("Training Start")
     avg_loss = []
-    
+
+    net.train()
+    for i in tqdm(range(epochs)):
+        output = net(X)
+        loss = loss_fn(output, y.reshape(-1,1))
+
+        loss.backward()
+        optimizer.step()
+        if i % 300 == 0:
+            print(loss)
+            print("Learn Rate:", optimizer.param_groups[0]["lr"])
+            lr_scheduler.step()
+    """
     for epoch in range(1, epochs + 1):
-        local_avg_loss = []
         print("Current Epoch:",epoch)
         print("Learn Rate:", optimizer.param_groups[0]["lr"])
         net.train()
         for i, (X,y) in tqdm(enumerate(dataloader_train)):
             net.zero_grad()
-
             output = net(X.to(device))
             y = y.to(device).float()
-            loss = loss_fn(output, y) 
+            loss = loss_fn(output, y.reshape(-1,1))
             loss.backward()
             optimizer.step()
-
-            local_avg_loss.append(loss.item())
-
-        avg_loss.append(sum(local_avg_loss) / len(local_avg_loss))
-        
-
+    """
             
-        torch.save(net.state_dict(), os.path.join(model_storage_dir,f"{epoch*len(test1_dataset)}.pth"))
+        #torch.save(net.state_dict(), os.path.join(model_storage_dir,f"{epoch*len(test1_dataset)}.pth"))
+        #lr_scheduler.step(avg_loss[-1])
             
-        lr_scheduler.step(avg_loss[-1])
-            
-        print("current avg loss:",avg_loss[-1] )
 
-        
-
-        net.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for i, (test_X,test_y) in enumerate(dataloader_test):
-                result = net(test_X.to(device))
-                predicted_class = torch.argmax(result, dim=1)
-                correct += int(sum(torch.where(predicted_class == test_y.to(device), 1 , 0)))
-                total += batch_size
-            print("Accuracy", round(correct/total, 5))
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i, (test_X,test_y) in enumerate(dataloader_test):
+            result = net(test_X.to(device))
+            predicted_class = torch.argmax(result, dim=1)
+            correct += int(sum(torch.where(predicted_class == test_y.to(device), 1 , 0)))
+            total += batch_size
+        print("Accuracy", round(correct/total, 5))
 
     end = time()
     print(round( (end - start)/60, 3), "Minutes")
-    plt.plot(avg_loss)
-    plt.show()
+    #plt.plot(avg_loss)
+    #plt.show()
